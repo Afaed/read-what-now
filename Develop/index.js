3971bda2252f23cf5a22a3c1893edbaaacba5edf
Admin <admin@admins-mbp.lan>
2022-01-09 21:09:01 -0800

    fleshed out the prompts, data, and the license functions. Also made the write file program work

--- Develop/index.js
+++ Develop/index.js
@@ -1,54 +1,71 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
-const  fs = require('fs')
-const os= require('os')
-const generatePage = require('./utils/generateMarkdown');
+const fs = require('fs')
+const generatePage = require('./utils/generateMarkdown')
 const { resolve } = require('path');
 var questions = [
-    {
+  {
     type: "input",
     name: "Title",
     message: "name?",
     validate: nameInput => {
       if (nameInput) {
-        return true; 
+        return true;
       } else {
-          console.log('Please enter your project title')
-          return false;
-        }
+        console.log('Please enter your project title')
+        return false;
       }
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "What is the description of the project?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your Github Username",
+    }
   },
   {
     type: "input",
-    name: "installation",
+    name: "description",
+    message: "What is the description of the project?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your Github Username",
+  },
+  {
+    type: "input",
+    name: "install",
     message: "How do you install your application?",
-     //after use a .then to convert it to a json and make a new file
-},
-{
-  type: "list",
-  name: "licence",
-  message: "what is the lisence of the project?",
-  choices: ["MIT", "Apache", "GNU", "Eclipse"] //after use a .then to convert it to a json and make a new file
-},
-{
-  type: "input",
-  name: "email",
-  message: "What is your Email address?",
-  //after use a .then to convert it to a json and make a new file
-}];
+    //after use a .then to convert it to a json and make a new file
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What is the lisence of the project?",
+    choices: ["MIT", "Apache", "GNU", "Eclipse", "None"] 
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your Email address?",
+    
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do i use your app?",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "List all your contributors."
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Write how to test your project here."
+  }
+
+];
 inquirer.prompt(questions)
-.then((answers) =>{
-  console.log(answers)})
+  .then((answers) => {
+    fs.writeFileSync("GENERATEDREADME.MD", generatePage.generateMarkdown(answers));
+  })
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -56,44 +73,7 @@ inquirer.prompt(questions)
     } else {
       // Something else went wrong
       console.log("Error")
-    }})
+    }
+  })
 
-  // TODO: Create a function to write README file
-const writeFile = fileContent => {
-  return new Promise ((resolve, reject) => {if (err){
-    reject(err)
-  }
-});
-function writeToFile(questions, data) {
-   const writeFile = fileContent => {
-     fs.writeFile(`./dist/index.html`, pageHTML, err => {
-     if (err) {
-       reject(err);
-       return;
-     }
-     resolve({ok: true,
-      message:'File created!',})
-     console.log('Congratulations! Your Readne has been realized! Leave.')
-
-     fs.copyFile('./src/style.css', './dist/style.css', err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Style sheet copied successfully!');
-
-   });
-});
-}}
-}
-// TODO: Create a function to initialize app
-function generatePage() {
-  module.exports = generatePage => {
-    console.log(questions);
-    
-    return ``;
-};
-}
-
-// Function call to initialize app
-init();
+  
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,53 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license = null){
-  return null}
+  if (license == "None") {
+    return ""
+  }
+  else if (license == "MIT") {
+    return 'https://opensource.org/licenses/MIT'
+  }
+  else if (license == "Apache") {
+    return 'https://www.apache.org/licenses/LICENSE-2.0'
+  }
+  else if (license === "GNU") {
+    return 'https://img.shields.io/badge/License-GPLv3-blue.svg'
+  }
+  else if (license === "Eclipse") {
+    return 'https://img.shields.io/badge/License-EPL_1.0-red.svg'
+  }
+  else if (license === "None") {
+    return ""
+  }
   return `http://img.shields.io/badge/license-${license}-blue.svg`
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license == "MIT"){
-  return 'https://opensource.org/licenses/MIT'}
-  else if (license == "Apache"){
-    return 'https://www.apache.org/licenses/LICENSE-2.0'
+
+  function renderLicenseLink(license) {
+    if (license == "MIT"){
+      return 'https://opensource.org/licenses/MIT'}
+    else if (license == "Apache"){
+      return 'https://www.apache.org/licenses/LICENSE-2.0'
+    }
+    else if (license === "GNU"){
+      return 'https://www.gnu.org/licenses/gpl-3.0.en.html'
+    }
+    else if (license === "Eclipse"){
+      return 'https://www.eclipse.org/legal/epl-2.0/'
+    }
+    else {
+      return ""
+    }
   }
-  else if (licence === "GNU"){
-    return 'https://img.shields.io/badge/License-GPLv3-blue.svg'
-  }
-  else if (licence === "Eclipse"){
-    return 'https://img.shields.io/badge/License-EPL_1.0-red.svg'
-  }
-  else return null
-}
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (renderLicenseBadge == renderLicenseLink(license), renderLicenseBadge(license)) {
-    return license
+  if (license == "None") {
+    return ""
   } else {
-    return null;}
-};
-var exampleData =
-{
-  title: "How to read",
-  lisence: "Apache",
-  description: "Reading",
-  install: "perform a git pull",
-  usage: "read",
-  contributors: "me",
-  test: "test",
-  questions: "How to contact me: email justchoy99@gmail.com",
+    return `This project is licensed under the [${license} license](${renderLicenseLink(license)}).`
+  };
 }
 
-console.log(generateMarkdown(exampleData))
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  [Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
-  
+  ![license badge] ${renderLicenseBadge(data.license)}
+
   ## Description 
   ${data.description}
   ## Table of Contents
@@ -63,15 +72,15 @@ function generateMarkdown(data) {
   ## Usage 
   ${data.usage}
   ## License 
-  This project is license under ${data.license}
+  ${renderLicenseSection(data.license)}
   ## Contributing 
   ${data.contributors}
   ## Tests
   ${data.test}
   ## Questions
-  ${data.questions}
+  ${data.email}
   If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports.generateMarkdown = generateMarkdown;
